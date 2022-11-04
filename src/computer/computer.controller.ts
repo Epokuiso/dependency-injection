fbf7a90b4cfc3d556ce0af9a0f6e9122a29f8da5
Epokuiso <epokuiso@gmail.com>
2022-11-04 13:02:31 +0100

    modules wrapup

--- src/computer/computer.controller.ts
+++ src/computer/computer.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { CpuService } from 'src/cpu/cpu.service';
+import { DiskService } from 'src/disk/disk.service';
 
 @Controller('computer')
-export class ComputerController {}
+export class ComputerController 
+{
+    constructor (
+        private cpuService : CpuService,
+        private diskService : DiskService
+    ) {}
+
+    @Get('run')
+    run ()
+    {
+        return [
+            this.cpuService.compute (15445, 6),
+            this.diskService.getData ()
+        ];
+    }
+
+}
--- src/cpu/cpu.service.ts
+++ src/cpu/cpu.service.ts
@@ -4,5 +4,13 @@ import { PowerService } from 'src/power/power.service';
 @Injectable()
 export class CpuService 
 {
-    constructor (public powerService: PowerService){}
+    constructor (private powerService: PowerService) {}
+
+    compute (a: number, b: number)
+    {
+        console.log (`Drawing 10 watts of power from PowerService`);
+        this.powerService.supplyPower (10);
+        return a + b;
+    }
+
 }
